9fabf834db5bcef4aa070b98f70917aebb4ab94c
Akshit1311 <a4748g@gmail.com>
2021-07-01 14:39:39 +0530

    add: routes as object

--- app.js
+++ app.js
@@ -7,34 +7,71 @@ app.use(express.static(__dirname + "/public"));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("home", { title: "Home" });
-});
-app.get("/contact", (req, res) => {
-  res.render("contact", { title: "Contact" });
-});
-app.get("/contribute", (req, res) => {
-  res.render("contribute", { title: "Contribute" });
-});
-app.get("/adopt", (req, res) => {
-  res.render("adopt", { title: "Adopt" });
-});
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
-});
-app.get("/gallery", (req, res) => {
-  res.render("gallery", { title: "Gallery" });
-});
-app.get("/team", (req, res) => {
-  res.render("team", { title: "Team" });
-});
-app.get("/blogs", (req, res) => {
-  res.render("blogs", { title: "Blogs" });
+const routesData = {
+  "/": {
+    page: "home",
+    data: { title: "Home" },
+  },
+  "/contact": {
+    page: "contact",
+    data: { title: "Contact" },
+  },
+  "/contribute": {
+    page: "contribute",
+    data: { title: "Contribute" },
+  },
+  "/adopt": {
+    page: "adopt",
+    data: { title: "Adopt" },
+  },
+  "/about": {
+    page: "about",
+    data: { title: "About" },
+  },
+  "/gallery": {
+    page: "gallery",
+    data: { title: "Gallery" },
+  },
+  "/team": {
+    page: "team",
+    data: { title: "Team" },
+  },
+};
+
+//Render Routes
+Object.keys(routesData).forEach((route) => {
+  const { page, data } = routesData[route];
+
+  return app.get(route, (req, res) => {
+    res.render(page, data);
+  });
 });
 
-app.use(function (req, res, next) {
-  res.status(404).render("notfound", { title: "Page Not Found" });
-});
+// app.get("/contact", (req, res) => {
+//   res.render("contact", { title: "Contact" });
+// });
+// app.get("/contribute", (req, res) => {
+//   res.render("contribute", { title: "Contribute" });
+// });
+// app.get("/adopt", (req, res) => {
+//   res.render("adopt", { title: "Adopt" });
+// });
+// app.get("/about", (req, res) => {
+//   res.render("about", { title: "About" });
+// });
+// app.get("/gallery", (req, res) => {
+//   res.render("gallery", { title: "Gallery" });
+// });
+// app.get("/team", (req, res) => {
+//   res.render("team", { title: "Team" });
+// });
+// app.get("/blogs", (req, res) => {
+//   res.render("blogs", { title: "Blogs" });
+// });
+
+// app.use(function (req, res, next) {
+//   res.status(404).render("notfound", { title: "Page Not Found" });
+// });
 
 const PORT = process.env.PORT || 3000;
 
